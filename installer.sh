e9bb504e9d8c68f6046473656e12ef66d2a62360
محمد جواد کریمی <40235152+im-JvD@users.noreply.github.com>
2025-09-17 22:04:15 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -20,7 +20,7 @@ show_menu() {
   clear
   echo -e "${BLUE}========================================${NC}"
   echo -e "${GREEN}  X-UI Reseller Reminder Bot Manager   ${NC}"
-  echo -e "${YELLOW}  BOT Version [${GREEN} v1.3.9 ]   ${NC}"
+  echo -e "${YELLOW}    BOT Version [${GREEN} v1.3.9 ${NC}]   "
   echo -e "${BLUE}========================================${NC}"
   echo -e ""
   echo -e "${GREEN}1)${NC} Install Bot"
