b5c6ad3a17bf0919f71a4e6e16d79b8fd9d20475
محمد جواد کریمی <40235152+im-JvD@users.noreply.github.com>
2025-10-12 17:44:59 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -66,7 +66,7 @@ install_bot() {
   cd "$INSTALL_DIR"
 
   echo -e "${BLUE}📦 Creating Python virtual environment${NC}"
-  apt install python3.10-venv
+  apt install python3.12-venv
   rm -rf .venv
   python3 -m venv .venv
   if [ ! -f ".venv/bin/activate" ]; then
