e415bb83e150300d0dccc7837300101a957cc5f9
محمد جواد کریمی <40235152+im-JvD@users.noreply.github.com>
2025-10-21 16:34:00 +0330

    Change report scheduling time to midnight Tehran time

--- bot.py
+++ bot.py
@@ -944,7 +944,7 @@ async def main():
         return
 
     # Schedule jobs
-    scheduler.add_job(send_full_reports, "cron", hour=20, minute=30)
+    scheduler.add_job(send_full_reports, "cron", hour=00, minute=00, timezone="Asia/Tehran")
     scheduler.add_job(check_for_changes, "interval", minutes=15)
     scheduler.add_job(api.login, "interval", hours=5, args=[LOGIN_URL])
     
